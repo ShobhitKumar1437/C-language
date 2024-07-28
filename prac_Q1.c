9d8da3b1f0906db6e63414beaa5fae2a0cfa78ec
SHOBHIT KUMAR <146829121+ShobhitKumar1437@users.noreply.github.com>
2024-07-28 11:53:36 +0530

    Add files via upload

---
+++ prac_Q1.c
@@ -0,0 +1,11 @@
+// Q.---> Write a program to calculate area of a square (side is given). 
+
+# include<stdio.h> 
+int main(){
+    int side ;
+    printf("enter side");
+    scanf("%d" , &side);
+
+    printf("area is : %d" , side * side);
+    return 0; 
+}
---
+++ prac_Q2.c
@@ -0,0 +1,11 @@
+// Q.--->  Write a program to find  area of a circle  (radius is given). 
+
+# include<stdio.h>
+int main(){
+    float radius;
+    printf("enter radius");
+    scanf("%f", &radius);
+
+    printf("area of circle  is : %f " , 3.14 * radius * radius );
+    return 0;
+}
